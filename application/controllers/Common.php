9aaf551bc9709bbb5e03f470f02e8be2805cba71
Kalpit Bhadani <bikalpitbhadani@gmail.com>
2020-10-14 16:37:35 +0530

    Kalpit

--- application/controllers/Common.php
+++ application/controllers/Common.php
@@ -14,7 +14,35 @@ public function _construct()
 	}
 	
 	public function facebookAds()
-	{		
-		$this->load->view('facebook');
+	{	
+		//fopen('assets/readme/tokenStore.txt');
+		if(file_exists("assets/readme/tokenStore.txt"))
+		{
+			$file = fopen('assets/readme/tokenStore.txt',"r");
+			$token =  fgets($file);
+			$pass_data = ['token'=>$token];
+			fclose($file);	
+		}
+		else
+		{
+			$pass_data = ['token'=>""];
+		}
+		
+		$this->load->view('facebook',$pass_data);
+	}
+	public function updatetoken()
+	{
+		$access_token = $this->input->post('access_token');
+		if(!empty($access_token))
+		{
+				$myfile = fopen("assets/readme/tokenStore.txt", "w");
+				fwrite($myfile, $access_token);
+				fclose($myfile);
+				echo "1";	
+		}
+		else
+		{
+			echo "0";
+		}
 	}
 }
