4c945beff18dcd9b50b96c7d15eee4f3c7a75e7c
vishi03 <vishalbimistry@yahoo.com>
2020-10-14 17:56:14 +0530

    config set

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/googleads/';
+$config['base_url'] = 'http://ggunew.bi-team.in/';
 
 /*
 |--------------------------------------------------------------------------
